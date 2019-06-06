4b86199d913cc71000b51b3c89ce0d59e0ec9071
Dmitry Bessonov <dmitrybessonov@icloud.com>
2019-06-06 13:58:45 +0300

    Updates

---
+++ precache-manifest.8d7bad8c32011bdcdcb8260356e47828.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202c13a3a88b95c3f780148e3f539a1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5083ef0ae0019624cff9",
+    "url": "/static/css/main.b7956f4b.chunk.css"
+  },
+  {
+    "revision": "2b45e18a26943891eaad",
+    "url": "/static/js/2.3f790490.chunk.js"
+  },
+  {
+    "revision": "5083ef0ae0019624cff9",
+    "url": "/static/js/main.a710d5fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64b6f4e92ee99d6e49e9d4d5f49b9223",
+    "url": "/static/media/Ammon.64b6f4e9.jpg"
+  },
+  {
+    "revision": "538f72aa1d8c330b52c4813539aa8330",
+    "url": "/static/media/PFAgoraSansPro-Light.538f72aa.ttf"
+  },
+  {
+    "revision": "f72a6a3458ec3a7e39d800134ea869a5",
+    "url": "/static/media/PFAgoraSansPro-Medium.f72a6a34.ttf"
+  },
+  {
+    "revision": "9fe653812ce0e20674b3e0d728ef1526",
+    "url": "/static/media/PFAgoraSansPro-Thin.9fe65381.ttf"
+  },
+  {
+    "revision": "215efe3eb8df2b98e7ceed6ecbe1ebb5",
+    "url": "/static/media/Renoir-compressor.215efe3e.jpeg"
+  },
+  {
+    "revision": "f38356bf347edfb38a93ab0144f05212",
+    "url": "/static/media/Renoir_01.f38356bf.jpg"
+  },
+  {
+    "revision": "8077e4c9be4be87d93bfd01df94312fb",
+    "url": "/static/media/Renoir_02.8077e4c9.jpg"
+  },
+  {
+    "revision": "31f962bd9b2bcf28f4d988cb59445842",
+    "url": "/static/media/Renoir_03.31f962bd.jpg"
+  },
+  {
+    "revision": "c2448da0194fc093543f4e718b08e681",
+    "url": "/static/media/Renoir_04.c2448da0.jpg"
+  },
+  {
+    "revision": "470a61b475def69af485c1236d0b1f0d",
+    "url": "/static/media/Renoir_05.470a61b4.jpg"
+  },
+  {
+    "revision": "8611848bd277cbf2a43c93ea0e2d5403",
+    "url": "/static/media/Renoir_06.8611848b.jpg"
+  },
+  {
+    "revision": "63c6fda5eb5c2452b56f52d0cceeba37",
+    "url": "/static/media/Renoir_07.63c6fda5.jpg"
+  },
+  {
+    "revision": "ad0d14e373a740f5e0067525a6551b73",
+    "url": "/static/media/Renoir_08.ad0d14e3.jpg"
+  },
+  {
+    "revision": "05e3167688de4a5c03651bb1786e888e",
+    "url": "/static/media/Renoir_09.05e31676.jpg"
+  },
+  {
+    "revision": "5a76e0f7312ba1fed7b6e03e6ac52c30",
+    "url": "/static/media/Renoir_10.5a76e0f7.jpg"
+  },
+  {
+    "revision": "6ca0f9a07d6ea4344e3360b2c30c6efb",
+    "url": "/static/media/Renoir_11.6ca0f9a0.jpg"
+  },
+  {
+    "revision": "42b44fd5d34aded71f913e7d57d2203c",
+    "url": "/static/media/Renoir_12.42b44fd5.jpg"
+  },
+  {
+    "revision": "841cf1f49012fdcc15bb5b533ed36eac",
+    "url": "/static/media/Renoir_13.841cf1f4.jpg"
+  },
+  {
+    "revision": "eabd07c7618b809f3f0df8878d7a7dad",
+    "url": "/static/media/Renoir_14.eabd07c7.jpg"
+  },
+  {
+    "revision": "adc5c14c1896e385cc6861da5835c62b",
+    "url": "/static/media/Renoir_15.adc5c14c.jpg"
+  },
+  {
+    "revision": "dca2cee0f909727a77a08d34932dd301",
+    "url": "/static/media/Renoir_16.dca2cee0.jpg"
+  }
+]);
